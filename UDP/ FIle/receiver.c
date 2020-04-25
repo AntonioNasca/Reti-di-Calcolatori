c51989cbce9759b1dfa20ab894ad9052e90fc424
Antonio <45246670+AntonioNasca@users.noreply.github.com>
2020-04-25 15:58:04 +0200

    Update and rename UDP/ FIle/receiver.c to  UDP/ FIle/receiver.c

--- UDP/ FIle/receiver.c
+++ UDP/ FIle/receiver.c
@@ -1,58 +1,52 @@
-/* UDP Sender */
+/* UDP Receiver */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 int main(int argc, char* argv[])
 { 
-  int sockfd; 
-  struct sockaddr_in remote_addr;
-  char buffer[300], c[4]={"fine"};
+  int sockfd;
+  struct sockaddr_in local_addr, remote_addr;
   socklen_t len = sizeof(struct sockaddr_in);
-  
+  char buffer[300];
   FILE *fp;
-  fp = fopen("testo.txt", "r");
-
+  fp=fopen("testo.txt","a");
   
-  if(argc < 3)
-  {  printf("Use: sender IP_dest PORT_dest");
+   
+  if(argc < 2)
+  {  printf("Sto ascoltando dalla porta");
    return 0;
   }  
   
   if((sockfd=socket(PF_INET,SOCK_DGRAM,0)) <0)
-  { printf("\nError in socket opening ... exit!");
+  { printf("\nErrore nell'apertura del socket ... exit!");
     return -1;
   }
-  
-  memset(&remote_addr,0,len);
-  remote_addr.sin_family = AF_INET;
-  inet_pton(AF_INET, argv[1], &(remote_addr.sin_addr)); 
-  remote_addr.sin_port=htons(atoi(argv[2]));
 
-  //INVIO IL FILE DI TEST
-    if (fp)
-    {
-        
-        while (!feof(fp))
-        { 
-            //Invio ogni riga del file
-            if (fgets(buffer, 300, fp) != NULL)
-            sendto(sockfd,buffer,300, 0, (struct sockaddr *) &remote_addr, sizeof(remote_addr));   
-             
-             
-            else
-            {
-                printf("errore!");
-                break;
-            }
-          
-        }
-      
-        fclose(fp);
-         
-    }
-    sendto(sockfd,c,sizeof(c), 0, (struct sockaddr *) &remote_addr, sizeof(remote_addr)); 
-    printf("Finito\n");
+ memset(&local_addr,0,sizeof(local_addr));
+ memset(&remote_addr,0,sizeof(remote_addr));
+
+ local_addr.sin_family = AF_INET;
+ local_addr.sin_port = htons(atoi(argv[1]));
+   
+
+bind(sockfd, (struct sockaddr *) &local_addr, len);
   
+    
+
+    printf("Aspetto il messaggio:\n");
+    
+    //fprintf(fp,"%d\n",end);
+   
+    for(;;)
+    { 
+        recvfrom(sockfd,&buffer,sizeof(buffer),0,(struct sockaddr *) &remote_addr,&len);
+        printf("%s",buffer); 
+       
+    }
+    
+    
+    
 }
